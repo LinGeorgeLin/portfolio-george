10db45125245eab880a69fb27c68d9af3223a893
George <glin7583@gmail.com>
2026-03-15 14:09:22 +0800

    20260315v4

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/home';
-import Works from './pages/works';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Works from "./pages/works";
+import ScrollToTop from "./components/ScrollToTop";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/works" element={<Works />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/works" element={<Works />} />
+      </Routes>
+    </>
   );
 }
